13cef12f43d282154a432fe2610ad12e82105f31
Rahul Jain <rahulkj@gmail.com>
2016-06-21 11:11:25 -0500

    Modified the plugin to output the orgs/spaces and their state

--- cf_buildpacks_usage.go
+++ cf_buildpacks_usage.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"strconv"
-	"strings"
 
 	"github.com/cloudfoundry/cli/plugin"
 )
@@ -12,23 +9,9 @@ import (
 // BuildpackUsage represents Buildpack Usage CLI interface
 type BuildpackUsage struct{}
 
-// AppSearchResults represents top level attributes of JSON response from Cloud Foundry API
-type AppSearchResults struct {
-	TotalResults int                  `json:"total_results"`
-	TotalPages   int                  `json:"total_pages"`
-	Resources    []AppSearchResources `json:"resources"`
-}
-
-// AppSearchResources represents resources attribute of JSON response from Cloud Foundry API
-type AppSearchResources struct {
-	Entity AppSearchEntity `json:"entity"`
-}
-
-// AppSearchEntity represents entity attribute of resources attribute within JSON response from Cloud Foundry API
-type AppSearchEntity struct {
-	Name              string `json:"name"`
-	Buildpack         string `json:"buildpack"`
-	DetectedBuildpack string `json:"detected_buildpack"`
+// Metadata is the data retrived from the response json
+type Metadata struct {
+	GUID string `json:"guid"`
 }
 
 // GetMetadata provides the Cloud Foundry CLI with metadata to provide user about how to use buildpack-usage command
@@ -59,50 +42,33 @@ func main() {
 // Run is what is executed by the Cloud Foundry CLI when the buildpack-usage command is specified
 func (c BuildpackUsage) Run(cli plugin.CliConnection, args []string) {
 	if args[0] == "buildpack-usage" {
-		res := c.GetAppData(cli)
-		c.PrintBuildpacks(res)
+		orgs := c.GetOrgs(cli)
+		spaces := c.GetSpaces(cli)
+		apps := c.GetAppData(cli)
+		c.PrintBuildpacks(orgs, spaces, apps)
 	}
 }
 
 // PrintBuildpacks prints the buildpack data to console
-func (c BuildpackUsage) PrintBuildpacks(res AppSearchResults) {
+func (c BuildpackUsage) PrintBuildpacks(orgs map[string]string, spaces map[string]SpaceSearchEntity, apps AppSearchResults) {
 	fmt.Println("")
 
 	fmt.Printf("Following is the table of apps and buildpacks app deployments\n\n")
 
-	fmt.Println("-------------------------------")
-	for _, val := range res.Resources {
+	fmt.Printf("| %10s | %30s | %30s | %10s | %250s |\n", "ORG", "SPACE", "APPLICATION", "STATE", "BUILDPACK")
+	fmt.Printf("| %10s | %30s | %30s | %10s | %250s |\n", "-----", "-----", "-----", "-----", "-----")
+
+	for _, val := range apps.Resources {
 		bp := val.Entity.Buildpack
 		if bp == "" {
 			bp = val.Entity.DetectedBuildpack
 		}
-		fmt.Printf("| %s - %s |\n", val.Entity.Name, bp)
-	}
-	fmt.Println("-------------------------------")
 
-}
+		space := spaces[val.Entity.SpaceGUID]
+		spaceName := space.Name
+		orgName := orgs[space.OrgGUID]
 
-// GetAppData requests all of the Application data from Cloud Foundry
-func (c BuildpackUsage) GetAppData(cli plugin.CliConnection) AppSearchResults {
-	var res AppSearchResults
-	res = c.UnmarshallAppSearchResults("/v2/apps?order-direction=asc&results-per-page=100", cli)
-
-	if res.TotalPages > 1 {
-		for i := 2; i <= res.TotalPages; i++ {
-			apiUrl := fmt.Sprintf("/v2/apps?order-direction=asc&page=%v&results-per-page=100", strconv.Itoa(i))
-			tRes := c.UnmarshallAppSearchResults(apiUrl, cli)
-			res.Resources = append(res.Resources, tRes.Resources...)
-		}
+		fmt.Printf("| %10s | %30s | %30s | %10s | %250s |\n", orgName, spaceName, val.Entity.Name, val.Entity.State, bp)
 	}
 
-	return res
-}
-
-func (c BuildpackUsage) UnmarshallAppSearchResults(apiUrl string, cli plugin.CliConnection) AppSearchResults {
-	var tRes AppSearchResults
-	cmd := []string{"curl", apiUrl}
-	output, _ := cli.CliCommandWithoutTerminalOutput(cmd...)
-	json.Unmarshal([]byte(strings.Join(output, "")), &tRes)
-
-	return tRes
 }
