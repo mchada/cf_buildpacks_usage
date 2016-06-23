d1b9732d013a53c6106debb98a5590c850d0fbde
Rahul Jain <rahulkj@gmail.com>
2016-06-22 22:14:07 -0500

    Adding the csv output flag

--- cf_buildpacks_usage.go
+++ cf_buildpacks_usage.go
@@ -26,9 +26,9 @@ func (c *BuildpackUsage) GetMetadata() plugin.PluginMetadata {
 		Commands: []plugin.Command{
 			{
 				Name:     "buildpack-usage",
-				HelpText: "Command to view buildpack usage in current CLI target context.",
+				HelpText: "Command to view all apps associated with a buildpack in current CLI target context.",
 				UsageDetails: plugin.Usage{
-					Usage: "buildpack-usage\n   cf buildpack-usage",
+					Usage: "cf buildpack-usage\n   cf buildpack-usage --csv",
 				},
 			},
 		},
@@ -45,15 +45,42 @@ func (c BuildpackUsage) Run(cli plugin.CliConnection, args []string) {
 		orgs := c.GetOrgs(cli)
 		spaces := c.GetSpaces(cli)
 		apps := c.GetAppData(cli)
-		c.PrintBuildpacks(orgs, spaces, apps)
+		if len(args) == 2 && args[1] == "--csv" {
+			c.PrintInCSVFormat(orgs, spaces, apps)
+		} else {
+			c.PrintInMarkDownFormat(orgs, spaces, apps)
+		}
+
 	}
 }
 
-// PrintBuildpacks prints the buildpack data to console
-func (c BuildpackUsage) PrintBuildpacks(orgs map[string]string, spaces map[string]SpaceSearchEntity, apps AppSearchResults) {
+// PrintInMarkDownFormat prints the buildpack data to console
+func (c BuildpackUsage) PrintInCSVFormat(orgs map[string]string, spaces map[string]SpaceSearchEntity, apps AppSearchResults) {
 	fmt.Println("")
 
-	fmt.Printf("Following is the table of apps and buildpacks app deployments\n\n")
+	fmt.Printf("Following is the csv output \n\n")
+
+	fmt.Printf("%s,%s,%s,%s,%s\n", "ORG", "SPACE", "APPLICATION", "STATE", "BUILDPACK")
+
+	for _, val := range apps.Resources {
+		bp := val.Entity.Buildpack
+		if bp == "" {
+			bp = val.Entity.DetectedBuildpack
+		}
+
+		space := spaces[val.Entity.SpaceGUID]
+		spaceName := space.Name
+		orgName := orgs[space.OrgGUID]
+
+		fmt.Printf("%s,%s,%s,%s,%s\n", orgName, spaceName, val.Entity.Name, val.Entity.State, bp)
+	}
+}
+
+// PrintInMarkDownFormat prints the buildpack data to console
+func (c BuildpackUsage) PrintInMarkDownFormat(orgs map[string]string, spaces map[string]SpaceSearchEntity, apps AppSearchResults) {
+	fmt.Println("")
+
+	fmt.Printf("Following is the markdown output\n\n")
 
 	fmt.Printf("| %10s | %30s | %30s | %10s | %250s |\n", "ORG", "SPACE", "APPLICATION", "STATE", "BUILDPACK")
 	fmt.Printf("| %10s | %30s | %30s | %10s | %250s |\n", "-----", "-----", "-----", "-----", "-----")
